6dbd4ac5f602133dc1321a49e36c1e2f38809d85
37952 <edwardfan3737@gmail.com>
2024-11-01 19:45:41 -0700

    v2

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -6,6 +6,10 @@ namespace myTiles {
     export const tile1 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
@@ -29,7 +33,7 @@ namespace myTiles {
 . . . . . . . . . . . . . . . . 
 `, [myTiles.transparency16], TileScale.Sixteen);
             case "level0":
-            case "level2":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000400010000000000000101010000000001000000000000000100000000000000000000000101010000000000000000000000000000000000000000000000000101010100000000000001010001000000000000000000000000010000000000000000000100010100000000000001000000000000000000000000000000000000000000000100000001000000000000000000000100000000010000000002000001010100000000000100000000010100000000000000000001010000010000000000000000000000010000000003030303030303030303030303030303`, img`
+            case "level2":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000006000000000000000000000000000400010000000000000101010000000001000000000700000100000000000000000000000101010700000000000007000000000000000000000000000700000101010100000000000001010001060000000000000600000000010000060606000000000100010100000000000001000000000000000000000600000000000000000000070100000001000000000000000000000100000000010000000002070001010100000000000107000500010100000000000000000001010000010000000000000000000000010000000003030303030303030303030303030303`, img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . 2 . . . . . . 2 2 2 . 
@@ -46,7 +50,7 @@ namespace myTiles {
 2 2 . . . . . . . . . 2 2 . . 2 
 . . . . . . . . . . . 2 . . . . 
 . . . . . . . . . . . . . . . . 
-`, [myTiles.transparency16,myTiles.tile1,sprites.builtin.forestTiles0,myTiles.tile2,sprites.swamp.swampTile13], TileScale.Sixteen);
+`, [myTiles.transparency16,myTiles.tile1,sprites.builtin.forestTiles0,myTiles.tile2,sprites.swamp.swampTile13,sprites.castle.tileDarkGrass2,myTiles.tile3,myTiles.tile4], TileScale.Sixteen);
         }
         return null;
     })
@@ -58,6 +62,10 @@ namespace myTiles {
             case "tile1":return tile1;
             case "myTile0":
             case "tile2":return tile2;
+            case "myTile1":
+            case "tile3":return tile3;
+            case "myTile2":
+            case "tile4":return tile4;
         }
         return null;
     })
